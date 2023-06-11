303706ec605820b8590918cb14840420197c5e1d
Neo <neochronos.chaos9@gmail.com>
2023-06-11 10:55:54 +0200

    [Test] testing another code

--- naive_malloc.c
+++ naive_malloc.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "malloc.h"
+
+**
+ * naive_malloc - simple version of malloc
+ * @size: size to allocate
+ *
+ * Return: pointer to allocated chunk
+ *
+void *naive_malloc(size_t size)
+{
+	mem_alloc_t *mem;
+
+	*Break*
+	mem = sbrk(0);
+
+	* Extension *
+        mem->size = size + sizeof(size_t) + 10;
+	mem->address = sbrk(mem->size);
+
+	return (mem->address);
+}*/
+
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * naive_malloc - simple version of malloc
@@ -11,14 +34,14 @@
  */
 void *naive_malloc(size_t size)
 {
-	mem_alloc_t *mem;
+	void *previous_break;
 
-	/*Break*/
-	mem = sbrk(0);
-
-	/* Extension */
-        mem->size = size + sizeof(size_t) + 10;
-	mem->address = sbrk(mem->size);
-
-	return (mem->address);
+	previous_break = sbrk(size);
+	/* check for error */
+	if (previous_break == (void *) -1)
+	{
+		/* on error malloc returns NULL */
+		return (NULL);
+	}
+	return (previous_break);
 }
